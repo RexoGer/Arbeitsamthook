f994777c005ff98ab2e9bf9a4cb1659834086c57
RexoGer <117946181+RexoGer@users.noreply.github.com>
2023-01-19 18:59:48 +0100

    Delete Project directory

--- Project/Utilities/Includes/IncludeImGui.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Include Libraries
-#include "../../Modules/GUI/imgui.h"
-#include "../../Modules/GUI/imgui_impl_win32.h"
-#include "../../Modules/GUI/imgui_impl_dx9.h"
--- Project/Utilities/Includes/IncludeOpenCV.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Include Libraries
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/ocl.hpp>
